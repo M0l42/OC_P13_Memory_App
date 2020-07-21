763abb357ab6cd8b94752eb1d4bb37180eaf3df9
M042 <boukobza.nathan@gmail.com>
2020-07-21 23:26:41 +0200

    fonctional app

--- memory_app/models.py
+++ memory_app/models.py
@@ -13,24 +13,51 @@ RANK = [
 ]
 
 
+class Category(models.Model):
+    name = models.CharField(verbose_name='name', max_length=200)
+    slug = models.SlugField(verbose_name='slug', max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Cards(models.Model):
     recto = models.CharField(verbose_name='recto', max_length=200)
     verso = models.CharField(verbose_name='verso', max_length=200)
 
+    def __str__(self):
+        return self.recto + " - " + self.verso
 
-class Desk(models.Model):
+
+class Deck(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
     name = models.CharField(verbose_name='name', max_length=200)
     cards = models.ManyToManyField(Cards)
+    created_at = models.DateField(auto_now_add=True)
+    favorite = models.BooleanField(verbose_name="favorite", default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return self.name
 
 
 class CardsState(models.Model):
     cards = models.ForeignKey(Cards, on_delete=models.CASCADE, default=None)
-    desk = models.ForeignKey(Desk, on_delete=models.CASCADE, default=None)
-    date = models.DateField(auto_now=False, auto_now_add=False)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, default=None)
+    date = models.DateField(auto_now=True)
     rank = models.IntegerField(
         verbose_name="rank",
-        default=0,
+        default=1,
         choices=RANK,
     )
     side = models.BooleanField(verbose_name="side")
+    new = models.BooleanField(verbose_name="new", default=True)
+
+
+class QuickModeDeck(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, default=None)
+    rank = models.IntegerField(
+        verbose_name="rank",
+        default=1,
+        choices=RANK,
+    )
